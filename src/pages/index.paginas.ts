d925573e798855e74fb8e0808cfcee5a7c491ad6
Sandy Atencio <satencioh96@gmail.com>
2018-03-31 11:20:49 -0500

    creando archvio de export para paginas

---
+++ src/pages/index.paginas.ts
@@ -0,0 +1,7 @@
+
+export { InicioPage } from "./inicio/inicio";
+export { AprenderPage } from "./aprender/aprender";
+export { PracticarPage } from "./practicar/practicar";
+export { QuizPage } from "./quiz/quiz";
+export { PilasPage } from "./pilas/pilas";
+export { ColasPage } from "./colas/colas";
