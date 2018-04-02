2d7ebc6780f084b8656b20744068b6d03b7ca914
Essa <sandres9017@gmail.com>
2018-04-01 19:55:08 -0500

    Se creo la logicay la parte visual del componente Aprender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { HttpClientModule  } from '@angular/common/http';
+
 import { MyApp } from './app.component';
 import { InicioPage, AprenderPage, PracticarPage, QuizPage, PilasPage, ColasPage, } from "../pages/index.paginas";
 
-
+/*Providers*/
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AprenderProvider } from '../providers/aprender/aprender';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ColasPage
   ],
   imports: [
+    HttpClientModule ,
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
@@ -36,7 +40,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AprenderProvider
   ]
 })
 export class AppModule {}
--- src/pages/index.paginas.ts
+++ src/pages/index.paginas.ts
@@ -3,5 +3,5 @@ export { InicioPage } from "./inicio/inicio";
 export { AprenderPage } from "./aprender/aprender";
 export { PracticarPage } from "./practicar/practicar";
 export { QuizPage } from "./quiz/quiz";
-export { PilasPage } from "./pilas/pilas";
-export { ColasPage } from "./colas/colas";
+export { PilasPage } from "./aprender/pilas/pilas";
+export { ColasPage } from "./aprender/colas/colas";
